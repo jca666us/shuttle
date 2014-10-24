a7a8da4d60d82f76d09009b2ec455865992408df
Ken Cox <kcox@barclaycardus.com>
2014-10-24 15:12:09 -0400

    handling edge cases of after last shuttle and before first

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,6 +2,8 @@
 var s = {
     fawleyTimes: ['0645', '0700', '0715', '0730', '0745', '0800', '0815', '0830', '0845', '0900', '0915', '0930', '0945', '1000', '1030', '1100', '1130', '1200', '1230', '1300', '1330', '1400', '1430', '1500', '1530', '1600', '1615', '1630', '1645', '1700', '1715', '1730', '1745', '1800', '1830'],
     juniperTimes: ['0655', '0710', '0725', '0740', '0755', '0810', '0825', '0840', '0855', '0910', '0925', '0940', '0955', '1010', '1040', '1110', '1140', '1210', '1240', '1310', '1340', '1410', '1440', '1510', '1540', '1610', '1625', '1640', '1655', '1710', '1725', '1840', '1855', '1910', '1940'],
+    //juniperTimes: ['1940'],
+
     currentMilitaryTime: function () {
         var now = new Date(),
         minutes = now.getMinutes(),
@@ -29,7 +31,7 @@ var s = {
                 console.log('No data for this location');
         }
 
-        for (i = 0; i < times.length - 1; i++) {
+        for (i = 0; i < times.length; i++) {
 
             if (parseInt(times[i]) >= s.currentMilitaryTime()){
                 return times[i];
@@ -38,25 +40,59 @@ var s = {
         return false;
     },
     minutesUntil: function (where) {
-        var nowHoursMinutes = s.currentMilitaryTime().match(/.{1,2}/g),
-            depHoursMinutes = s.nextShuttleTime(where).match(/.{1,2}/g),
-            hoursNow = parseInt(nowHoursMinutes[0]),
-            minutesNow = parseInt(nowHoursMinutes[1]),
-            hoursNext = parseInt(depHoursMinutes[0]),
+        var nowHoursMinutes,
+            depHoursMinutes,
+            hoursNow,
+            minutesNow,
+            hoursNext,
+            minutesNext;
+
+        if (s.nextShuttleTime(where)) {
+            /* If there is a next shuttle */
+            nowHoursMinutes = s.currentMilitaryTime().match(/.{1,2}/g);
+            depHoursMinutes = s.nextShuttleTime(where).match(/.{1,2}/g);
+            hoursNow = parseInt(nowHoursMinutes[0]);
+            minutesNow = parseInt(nowHoursMinutes[1]);
+            hoursNext = parseInt(depHoursMinutes[0]);
             minutesNext = parseInt(depHoursMinutes[1]);
 
-        /* next shuttle is in the next hour */
-        if (hoursNow === hoursNext && minutesNow === minutesNext) {
-            return 'NOW!';
-        } else if (hoursNow < hoursNext) {
-            return 60 - minutesNow + minutesNext + ' Minutes';
-        /* next shuttle is in the same hour */
+            /* next shuttle is now */
+            if (hoursNow === hoursNext && minutesNow === minutesNext) {
+                return 'NOW!';
+            /* next shuttle is > an hour in the future */
+            } else if (hoursNow + 1 < hoursNext) {
+                if (hoursNext > 12) {
+                    hoursNext = hoursNext - 12;
+                }
+                return 'At ' + hoursNext + ':' + minutesNext;
+            /* next shuttle is in the next hour */
+            } else if (hoursNow < hoursNext) {
+                return 60 - minutesNow + minutesNext + ' Minutes';
+            /* next shuttle is in the same hour */
+            } else {
+                return minutesNext - minutesNow + ' Minutes';
+            }
+
         } else {
-            return minutesNext - minutesNow + ' Minutes';
+            /* there is no next shuttle */
+            return 'No Service';
         }
+
+        
+    },
+
+    display: function () {
+        $('#fawleyNextShuttle').html(s.minutesUntil('fawley'));
+        $('#juniperNextShuttle').html(s.minutesUntil('juniper'));
     }
 };
 
-$('#fawleyNextShuttle').html(s.minutesUntil('fawley'));
-$('#juniperNextShuttle').html(s.minutesUntil('juniper'));
+/* Initial Display */
+s.display();
+
+/* Refresh loop */
+setInterval(function() {
+    s.display();
+}, 5000);
+
 
