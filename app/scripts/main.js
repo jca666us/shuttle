f458027047007ab478b289400f5d68e7ebf49b2a
Ken Cox <kcox@barclaycardus.com>
2014-10-24 14:25:45 -0400

    Basic UI and multiple locations handling

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,7 @@
 'use strict';
 var s = {
-    fawleyTimes: [
-        '0645', '0700', '0715', '0730', '0745', '0800', '0815', '0830', '0845', '0900', '0915', '0930', '0945', '1000', '1030', '1100', '1130', '1200', '1230', '1300', '1330', '1400', '1430', '1500', '1530', '1600', '1615', '1630', '1645', '1700', '1715', '1730', '1745', '1800', '1830'
-    ],
+    fawleyTimes: ['0645', '0700', '0715', '0730', '0745', '0800', '0815', '0830', '0845', '0900', '0915', '0930', '0945', '1000', '1030', '1100', '1130', '1200', '1230', '1300', '1330', '1400', '1430', '1500', '1530', '1600', '1615', '1630', '1645', '1700', '1715', '1730', '1745', '1800', '1830'],
+    juniperTimes: ['0655', '0710', '0725', '0740', '0755', '0810', '0825', '0840', '0855', '0910', '0925', '0940', '0955', '1010', '1040', '1110', '1140', '1210', '1240', '1310', '1340', '1410', '1440', '1510', '1540', '1610', '1625', '1640', '1655', '1710', '1725', '1840', '1855', '1910', '1940'],
     currentMilitaryTime: function () {
         var now = new Date(),
         minutes = now.getMinutes(),
@@ -16,34 +15,48 @@ var s = {
 
         return hours + minutes;
     },
-    nextShuttleTime: function () {
-        var i;
-        for (i = 0; i < s.fawleyTimes.length - 1; i++) {
+    nextShuttleTime: function (where) {
+        var i, times;
 
-            if (parseInt(s.fawleyTimes[i]) > s.currentMilitaryTime()){
-                return s.fawleyTimes[i];
+        switch (where) {
+            case 'fawley':
+                times = s.fawleyTimes;
+                break;
+            case 'juniper':
+                times = s.juniperTimes;
+                break;
+            default:
+                console.log('No data for this location');
+        }
+
+        for (i = 0; i < times.length - 1; i++) {
+
+            if (parseInt(times[i]) >= s.currentMilitaryTime()){
+                return times[i];
             }
         }
         return false;
     },
-    minutesUntil: function () {
+    minutesUntil: function (where) {
         var nowHoursMinutes = s.currentMilitaryTime().match(/.{1,2}/g),
-            depHoursMinutes = s.nextShuttleTime().match(/.{1,2}/g),
+            depHoursMinutes = s.nextShuttleTime(where).match(/.{1,2}/g),
             hoursNow = parseInt(nowHoursMinutes[0]),
             minutesNow = parseInt(nowHoursMinutes[1]),
             hoursNext = parseInt(depHoursMinutes[0]),
             minutesNext = parseInt(depHoursMinutes[1]);
 
         /* next shuttle is in the next hour */
-        if (hoursNow < hoursNext) {
-            return 60 - minutesNow + minutesNext;
+        if (hoursNow === hoursNext && minutesNow === minutesNext) {
+            return 'NOW!';
+        } else if (hoursNow < hoursNext) {
+            return 60 - minutesNow + minutesNext + ' Minutes';
         /* next shuttle is in the same hour */
         } else {
-            return minutesNext - minutesNow;
+            return minutesNext - minutesNow + ' Minutes';
         }
-
-        
     }
 };
 
-console.log(s.minutesUntil());
+$('#fawleyNextShuttle').html(s.minutesUntil('fawley'));
+$('#juniperNextShuttle').html(s.minutesUntil('juniper'));
+
