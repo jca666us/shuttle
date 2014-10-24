22bbf9792587245c98c0fe475856a40f8bcff1b7
Ken Cox <kcox@barclaycardus.com>
2014-10-24 16:26:57 -0400

    fiddling with tests and adding todos

--- test/spec/test.js
+++ test/spec/test.js
@@ -3,11 +3,20 @@
 (function () {
   'use strict';
 
-  describe('Give it some context', function () {
-    describe('maybe a bit more context here', function () {
-      it('should run here few assertions', function () {
+  describe('Next shuttle calculator', function () {
+      it('should convert to military time', function () {
+        /*var now = new Date(2014, 9, 24, 4, 16),
+        minutes = now.getMinutes(),
+        hours = now.getHours().toString();
+        
+        if (minutes < 10) {
+            minutes = '0' + minutes;
+        } else {
+            minutes = minutes.toString();
+        }
 
+        assert(hours + minutes) === '0416';
+        */
       });
-    });
   });
 })();
