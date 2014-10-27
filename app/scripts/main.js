438854def9ae1f0248df06ae92939305b3b9f05f
Ken Cox <kcox@barclaycardus.com>
2014-10-27 13:22:42 -0400

    added chrome grunt plugin and some delinting

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var s = {
+    //TODO: Store data in a json file
     fawleyTimes: ['0645', '0700', '0715', '0730', '0745', '0800', '0815', '0830', '0845', '0900', '0915', '0930', '0945', '1000', '1030', '1100', '1130', '1200', '1230', '1300', '1330', '1400', '1430', '1500', '1530', '1600', '1615', '1630', '1645', '1700', '1715', '1730', '1745', '1800', '1830'],
     juniperTimes: ['0655', '0710', '0725', '0740', '0755', '0810', '0825', '0840', '0855', '0910', '0925', '0940', '0955', '1010', '1040', '1110', '1140', '1210', '1240', '1310', '1340', '1410', '1440', '1510', '1540', '1610', '1625', '1640', '1655', '1710', '1725', '1840', '1855', '1910', '1940'],
     // TODO: Add third pickup location
@@ -8,9 +9,9 @@ var s = {
 
     currentMilitaryTime: function () {
         var now = new Date(),
-        //var now = new Date(2014, 9, 27, 8, 55),
-        minutes = s.addLeadingZeros(now.getMinutes()),
-        hours = s.addLeadingZeros(now.getHours());
+            //var now = new Date(2014, 9, 27, 8, 55),
+            minutes = s.addLeadingZeros(now.getMinutes()),
+            hours = s.addLeadingZeros(now.getHours());
         return hours + minutes;
     },
     addLeadingZeros: function (n) {
@@ -27,19 +28,19 @@ var s = {
         //TODO: Only show times on weekdays
 
         switch (where) {
-            case 'fawley':
-                times = s.fawleyTimes;
-                break;
-            case 'juniper':
-                times = s.juniperTimes;
-                break;
-            default:
-                console.log('Invalid shuttle location');
+        case 'fawley':
+            times = s.fawleyTimes;
+            break;
+        case 'juniper':
+            times = s.juniperTimes;
+            break;
+        default:
+            console.log('Invalid shuttle location');
         }
 
         for (i = 0; i < times.length; i++) {
 
-            if (parseInt(times[i]) >= s.currentMilitaryTime()){
+            if (parseInt(times[i], 10) >= s.currentMilitaryTime()) {
                 return times[i];
             }
         }
@@ -57,11 +58,11 @@ var s = {
             /* If there is a next shuttle */
             nowHoursMinutes = s.currentMilitaryTime().match(/.{1,2}/g);
             depHoursMinutes = s.nextShuttleTime(where).match(/.{1,2}/g);
-            hoursNow = parseInt(nowHoursMinutes[0]);
-            minutesNow = parseInt(nowHoursMinutes[1]);
-            hoursNext = parseInt(depHoursMinutes[0]);
-            minutesNext = parseInt(depHoursMinutes[1]);
-            
+            hoursNow = parseInt(nowHoursMinutes[0], 10);
+            minutesNow = parseInt(nowHoursMinutes[1], 10);
+            hoursNext = parseInt(depHoursMinutes[0], 10);
+            minutesNext = parseInt(depHoursMinutes[1], 10);
+
             /* DEBUGGING
             console.log('%c nowHoursMinutes = ' + nowHoursMinutes, 'color: #ff0000;');
             console.log('%c depHoursMinutes = ' + depHoursMinutes, 'color: #006600;');
@@ -91,7 +92,7 @@ var s = {
             return 'No Service';
         }
 
-        
+
     },
 
     display: function () {
@@ -104,7 +105,7 @@ var s = {
 s.display();
 
 /* Refresh loop */
-setInterval(function() {
+setInterval(function () {
     s.display();
 }, 5000);
 
