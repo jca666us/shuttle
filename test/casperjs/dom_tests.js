26c00a8864ebd09d23eecc2ca616d91d7fe35dd1
Ken Cox <ken@kencox.net>
2014-11-29 19:23:10 -0500

    second casper dom test

--- test/casperjs/dom_tests.js
+++ test/casperjs/dom_tests.js
@@ -1,8 +1,14 @@
 /*jshint strict:false*/
 /*global CasperError, casper, console, phantom, require*/
-casper.test.begin('Shuttle Tests', 1, function(test) {
+casper.test.begin('Shuttle Tests', 2, function(test) {
     casper.start('http://localhost:9000', function() {
-        test.assertTitle('Shuttle');
+
+        test.assertTitle('Shuttle', 'Page has the correct title');
+
+        test.assertEval(function(){
+        	return __utils__.findAll('tr').length > 0;
+        }, 'app is displaying at least one row of data');
+        
     });
 
     casper.run(function() {
